2b79f30ef9e7d65d4f951f8595ba511c525c300b
DenisLohvin <l.denis.r.work@gmail.com>
2022-11-11 15:46:32 +0200

    Декларация переменных за пределы main Функция вычитания и деления добавлены

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,23 +1,35 @@
 package app;
 
 public class Main {
-    static int a = 8;
-    static int b = 2;
+    static int a;
+    static int b;
+
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-
+        a = 8;
+        b = 2;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int z = subtraction(a,b);
+        System.out.println(z);
+        int x = division(a,b);
+        System.out.println(x);
     }
-
     private static int add(int a, int b) {
         return a + b;
     }
 
     private static int multiply(int a, int b) {
         return a * b;
+    }
+
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
+    private static int division (int a, int b) {
+        return a / b;
     }
 }
